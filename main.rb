e240c05feb2b696ca48ff020391b0ec561f74f0e
jaqque <jaqque@gmail.com>
2019-07-13 10:42:20 -0700

    Add repl.it entry point

---
+++ main.rb
@@ -0,0 +1,6 @@
+puts "oh hey i read the schematica-materials.txt file directly"
+puts "i hope it is a part of this repo, other wise bad things may happen"
+puts
+puts "Bad Things."
+
+eval File.read("shulkerthingbutthistimeinruby.rb")
